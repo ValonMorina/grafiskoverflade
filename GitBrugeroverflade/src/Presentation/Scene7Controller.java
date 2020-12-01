2b5e82edd6f3ae1f0046d4dee1f423c2a0ea954b
Jespercommerou <71772076+Jespercommerou@users.noreply.github.com>
2020-12-01 11:04:38 +0100

    Controller 7

--- GitBrugeroverflade/src/Presentation/Scene7Controller.java
+++ GitBrugeroverflade/src/Presentation/Scene7Controller.java
@@ -1,4 +1,69 @@
 package Presentation;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+
 public class Scene7Controller {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private ImageView character;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button nailbutton;
+
+
+    @FXML
+    void keyPressed(KeyEvent event) throws InterruptedException, IOException {
+
+        switch (event.getCode()) {
+            case RIGHT:
+                character.setX(character.getX()+10);
+                break;
+
+            case LEFT:
+                character.setX(character.getX()-10);
+                break;
+
+            case UP:
+                character.setY(character.getY()-10);
+
+                break;
+
+            case DOWN:
+                character.setY(character.getY()+10);
+                break;
+
+            case N:
+                Main.setRoot("scene2");
+                System.out.println("Scene loaded successfully");
+                break;
+
+            default:
+                break;
+        }
+
+
+
+        System.out.println("-----------------------------");
+        System.out.println("X-værdi: " + character.getX());
+        System.out.println("Y værdi: " + character.getY());
+    }
 }
+
