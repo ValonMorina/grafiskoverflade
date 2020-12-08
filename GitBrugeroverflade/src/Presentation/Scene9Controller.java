f9ee9aac2b59d4ed21806e54bccbef4d1f347360
Victor <woydowskidralle@gmail.com>
2020-12-08 14:00:39 +0100

    talk

--- GitBrugeroverflade/src/Presentation/Scene9Controller.java
+++ GitBrugeroverflade/src/Presentation/Scene9Controller.java
@@ -6,10 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
@@ -22,9 +19,9 @@ import java.util.ResourceBundle;
 
 public class Scene9Controller implements Initializable {
     public MenuButton menuButton2;
-    public MenuItem talkbutton;
+    public MenuItem talkButton;
     public MenuItem buildbutton;
-
+    public TextArea textbox;
 
     @FXML
     private AnchorPane pane;
@@ -47,36 +44,35 @@ public class Scene9Controller implements Initializable {
 
         switch (event.getCode()) {
             case RIGHT:
-                character.setX(character.getX()+10);
+                character.setX(character.getX() + 10);
                 break;
 
             case LEFT:
-                character.setX(character.getX()-10);
-                if(character.getX()<-30) {
+                character.setX(character.getX() - 10);
+                if (character.getX() < -30) {
                     Main.setRoot("scene8");
                 }
                 break;
 
             case UP:
-                character.setY(character.getY()-10);
-                if(character.getY()<-320) {
+                character.setY(character.getY() - 10);
+                if (character.getY() < -320) {
                     Main.setRoot("scene5");
                 }
                 break;
 
             case DOWN:
-                character.setY(character.getY()+10);
+                character.setY(character.getY() + 10);
                 break;
 
             case ENTER:
-                if(character.getX() >= 485 && character.getX() < 520 ) {
-                  menuButton2.fire();
+                if (character.getX() >= 485 && character.getX() < 520) {
+                    menuButton2.fire();
                 }
                 break;
             default:
                 break;
         }
-
 
 
         System.out.println("-----------------------------");
@@ -88,6 +84,15 @@ public class Scene9Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
-}
 
+    public void handler(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == talkButton) {
+            textbox.setVisible(true);
+            textbox.setText("My name is Jennifer");
+            KeyFrame keyframe = new KeyFrame(Duration.seconds(3), actionEvent1 -> textbox.setVisible(false)); // With Lambda you can use methods as arguments
+            Timeline timeline = new Timeline(keyframe);
+            timeline.play();
+        }
+    }
+}
 
